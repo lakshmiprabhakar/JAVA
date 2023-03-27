bd4fd29352015206f161450ed60c1e0a5d53b55c
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2023-03-27 15:24:10 +0530

    Add files via upload

---
+++ CpuInfo.java
@@ -0,0 +1,36 @@
+class cpu {
+	double price;
+	String name;
+	
+	cpu(double p,String nam) {
+	 	price=p;
+	 	name=nam;
+	}
+	class processor {
+		int no_of_cores;
+		String manufacturer;
+		
+		processor(int cores,String man) {
+			no_of_cores = cores;
+			manufacturer = man;
+		}
+	}
+	static class Ram {
+	
+		String memory;
+		String manufacturer;
+		
+		Ram(String mem,String man) {
+			memory = mem;
+			manufacturer = man;
+		}
+	}
+
+}
+public class CpuInfo {
+	public static void main(String args[]) {
+		cpu c =new cpu()
+	
+	}
+
+}
