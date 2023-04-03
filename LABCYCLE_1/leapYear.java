d7ee305ed729fb4c6c137691f82110628423ac56
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2023-04-03 14:31:11 +0530

    Add files via upload

---
+++ LABCYCLE_1/leapYear.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class setYear{
+	Scanner s = new Scanner(Sytsem.in);
+	int start;
+	int end;
+	
+	setYear() {
+		System.out.println("Enter start year : ");
+		start=s.nextInt();
+		System.out.println("Enter end year : ");
+		end=s.nextInt();	
+	}
+	void checkYear() {
+		System.out.println("Leap years b/w " + start + "and" + end + "are : " );
+		for(int i = start; i<=end; i++){
+		   if(i%4==0 && i%100!=0 || i%400==0){
+		      System.out.println(i + "\t");
+		    } 
+		
+		}
+	
+	}
+
+}
+public class 
