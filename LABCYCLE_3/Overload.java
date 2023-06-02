d0b769193cb94bdde07340f938b64cdc3741a3fa
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2023-06-02 15:36:09 +0530

    Add files via upload

--- LABCYCLE_3/Overload.java
+++ LABCYCLE_3/Overload.java
@@ -1,51 +1,51 @@
-class OverloadDemo
 
-    {
+import java.util.*;
 
-        void area(float x)
-
-        {
-
-            System.out.println("the area of the square is "+Math.pow(x, 2)+" sq units");
-
-        }
-
-        void area(float x, float y)
-
-        {
-
-            System.out.println("the area of the rectangle is "+x*y+" sq units");
-
-        }
-
-        void area(double x)
-
-        {
-
-            double z = 3.14 * x * x;
-
-            System.out.println("the area of the circle is "+z+" sq units");
-
-        }
-
-    }
-
-    class Overload 
-
-    {
-
-         public static void main(String args[]) 
-
-    	{
-
-    	   OverloadDemo ob = new OverloadDemo();
-
-    	   ob.area(1,2);
-
-    	   ob.area(10,20);
-
-    	   ob.area(30,78);
-
-            }
-
-    }
+public class Overload {
+	int a;
+	int area;
+	double y;
+	
+	void area(int l,int b) {
+		int length = l;
+		int breadth = b;
+		a=length*breadth;
+		System.out.println("->Area: "+a);
+	}
+	
+	void area(int s) {
+		int side = s;
+		area = side * side;
+		System.out.println("->Area: "+area);	
+	}
+	
+	
+	
+	void area(double r) {
+	    double radius = r;
+		y = 3.14*radius*radius;
+		System.out.println("->Area: "+y);	
+	}
+	
+	
+public static void main(String[] args)	{
+	Overload obj = new Overload();
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Area of different shapes using overloaded functions");
+	System.out.println("\n____RECTANGLE____");
+	System.out.println("Enter the length and breadth:");
+	int l = sc.nextInt();
+	int b = sc.nextInt();
+	obj.area(l,b);
+	System.out.println("\n____SQUARE____");
+	System.out.println("->Enter the side:");
+	int s = sc.nextInt();
+	obj.area(s);
+	System.out.println("\n____CIRCLE____");
+	System.out.println("->Enter the radius:");
+	double r = sc.nextDouble();
+	obj.area(r);
+	sc.close();
+  }
+	
+}
